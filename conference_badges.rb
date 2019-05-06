2f5e3a9529069579f7be20bbfe7172717794f424
Avitosh Totaram <a.totaram@gmail.com>
2019-05-06 00:27:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,8 @@ def badge_maker(name)
   return "Hello, my name is #{name}." 
 end
 
-def batch_badge_creator(array_of_names)
+def batch_badge_creator(attendees)
+  badges = attendees.push(badge_maker)
   
 end
 
